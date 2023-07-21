200b3d668945aafc4df379692bfabfd0714cf9f7
bforester <bogdan.forest.job@gmail.com>
2023-07-21 23:49:25 +0300

    Creating a book entity

--- domain/store/Book.cs
+++ domain/store/Book.cs
@@ -4,7 +4,15 @@ namespace Store
 { 
     public class Book
     {
+        public int ID { get; }
 
+        public string Title { get; }
+
+        public Book(int id, string title) 
+        {
+            ID = id;
+            Title = title;
+        }
     }
 
 }
