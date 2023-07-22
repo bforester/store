b630167f292fe0c82f5a9aa357dae6595fee02f2
bforester <bogdan.forest.job@gmail.com>
2023-07-22 21:42:08 +0300

    Add interface a book repository

---
+++ domain/store/IBookRepository.cs
@@ -0,0 +1,14 @@
+﻿using Store;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace store
+{
+    public interface IBookRepository
+    {
+        Book[] GetAllByTitle(string titlePart);
+    }
+}
