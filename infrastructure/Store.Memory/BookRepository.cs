17f758084a485777f3cae46fd6a82311308065da
bforester <bogdan.forest.job@gmail.com>
2023-07-22 22:09:48 +0300

    Create web application

---
+++ infrastructure/Store.Memory/BookRepository.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using store;
+
+namespace Store.Memory
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly Book[] books = new[]
+        { 
+            new Book(1, "Clean Code."),
+            new Book(2, "CLR via C#."),
+            new Book(3, "Color of Wisard.")
+        };
+
+        public Book[] GetAllByTitle(string titlePart)
+        {
+            return books.Where(book => book.Title.Contains(titlePart))
+                        .ToArray();
+        }
+    }
+}
